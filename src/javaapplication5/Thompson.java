25f39dc8b5e611eb5f0871ac93aec59a7af6b0e3
Unknown <peinrinegan18@gmail.com>
2018-10-14 01:10:59 -0300

    Reemplazo de int a string para los estados

--- src/javaapplication5/Thompson.java
+++ src/javaapplication5/Thompson.java
@@ -1,3 +1,5 @@
+package javaapplication5;
+
 
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -9,11 +11,11 @@ public class Thompson{
             (state from, symbol of tranistion path, state to)
     */
     public static class Trans{
-        private int state_from; // Estado inicial
-        private int state_to; // Estado final
+        private String state_from; // Estado inicial
+        private String state_to; // Estado final
         private char trans_symbol; // Simbolo de transicion
 
-        public Trans(int v1, int v2, char sym){
+        public Trans(String v1, String v2, char sym){
             this.state_from = v1;
             this.state_to = v2;
             this.trans_symbol = sym;
@@ -25,39 +27,52 @@ public class Thompson{
             Finite Automata. Will use this to better combine the states.
     */
     public static class NFA{
-        public ArrayList <Integer> states; // conjunto de estados
+        public ArrayList <String> states; // conjunto de estados
         public ArrayList <Trans> transitions; // Conjunto de transiciones
-        public int final_state; // Estado final del automata
+        public String final_state; // Estado final del automata
+        public String estado;
         
         public NFA(){
-            this.states = new ArrayList <Integer> (); //Crear el arreglo de enteros para los estados
+            this.states = new ArrayList <String> (); //Crear el arreglo de enteros para los estados
             this.transitions = new ArrayList <Trans> (); //Crear un arreglo de tipo transicion
-            this.final_state = 0;// el estado final inicial es 0
+            this.final_state = "q0";// el estado final inicial es 0
         }
         public NFA(int size){
-            this.states = new ArrayList <Integer> ();
+            this.states = new ArrayList <String> ();
             this.transitions = new ArrayList <Trans> ();
-            this.final_state = 0;
+            this.final_state = "q0";
             this.setStateSize(size);// Asignar la cantidad de estados
         }
         public NFA(char c){
-            this.states = new ArrayList<Integer> ();
+            this.states = new ArrayList<String> ();
             this.transitions = new ArrayList <Trans> ();
             this.setStateSize(2);
-            this.final_state = 1;
-            this.transitions.add(new Trans(0, 1, c));// crea la transicion del 0 al 1 usando el caracter c
+            this.final_state = "q1";
+            this.transitions.add(new Trans("q0","q1", c));// crea la transicion del 0 al 1 usando el caracter c
         }
 
         public void setStateSize(int size){ // metodo que llena el arreglo de estados con la cantidad indicada
             for (int i = 0; i < size; i++)
-                this.states.add(i);
+                
+                estado="q"+Integer.toString(i);
+            System.out.println("Cantidad de estados allenar?:"+size);
+                this.states.add(estado);
         }
 
         public void display(){ // Metodo que imprime las transiciones del automata convertido
+            String k="";
             for (Trans t: transitions){
                 System.out.println("("+ t.state_from +", "+ t.trans_symbol +
                     ", "+ t.state_to +")");
-            }    
+                
+            }
+            System.out.println("Tamaño de estados: "+states.size());
+            for(int i =0;i<states.size();i++)
+            {
+                k=states.get(i)+","+k;
+            }
+            System.out.println("AFND:");
+            System.out.println("K={"+k+"}");
         }
     }
 
@@ -67,25 +82,25 @@ public class Thompson{
     */
     public static NFA kleene(NFA n){ //metodo para la estrella de kleene que recibe un objeto tipo NFA
         NFA result = new NFA(n.states.size()+2); // crea un nuevo NFA 
-        result.transitions.add(new Trans(0, 1, '_')); // new trans for q0 ,agrega transicion del 0 al 1 usando el vacio
+        result.transitions.add(new Trans("q0","q1", '_')); // new trans for q0 ,agrega transicion del 0 al 1 usando el vacio
 
         // copy existing transisitons
         for (Trans t: n.transitions){
-            result.transitions.add(new Trans(t.state_from + 1, //crea mas transiciones apartir de la inicial
+            result.transitions.add(new Trans(Integer.parseInt(t.state_from.substring(1)) + 1, //crea mas transiciones apartir de la inicial
             t.state_to + 1, t.trans_symbol));
         }
         
         // add empty transition from final n state to new final state.
-        result.transitions.add(new Trans(n.states.size(), 
-            n.states.size() + 1, '_')); //Agregar transicion vacio del estado n al nuevo estado final
+        result.transitions.add(new Trans("q"+Integer.toString(n.states.size()), 
+            "q"+Integer.toString(n.states.size() + 1), '_')); //Agregar transicion vacio del estado n al nuevo estado final
         
         // Loop back from last state of n to initial state of n.
-        result.transitions.add(new Trans(n.states.size(), 1, '_'));
+        result.transitions.add(new Trans("q"+Integer.toString(n.states.size()), "q"+1, '_'));
 
         // Add empty transition from new initial state to new final state.
-        result.transitions.add(new Trans(0, n.states.size() + 1, '_'));
+        result.transitions.add(new Trans("q0", "q"+Integer.toString(n.states.size() + 1), '_'));
 
-        result.final_state = n.states.size() + 1;
+        result.final_state = "q"+Integer.toString(n.states.size() + 1);
         return result;
     }
 
@@ -98,16 +113,16 @@ public class Thompson{
 
         // copy NFA m's transitions to n, and handles connecting n & m
         for (Trans t: m.transitions){
-            n.transitions.add(new Trans(t.state_from + n.states.size()-1,
-                t.state_to + n.states.size() - 1, t.trans_symbol));
+            n.transitions.add(new Trans(t.state_from.charAt(0) + Integer.toString(n.states.size()-1),//Ojo aquí , estamos pasando
+                t.state_to.charAt(0) + Integer.toString(n.states.size() - 1), t.trans_symbol));
         }
 
         // take m and combine to n after erasing inital m state
-        for (Integer s: m.states){
+        for (String s: m.states){
             n.states.add(s + n.states.size() + 1);
         }
         
-        n.final_state = n.states.size() + m.states.size() - 2;
+        n.final_state = "q"+Integer.toString(n.states.size() + m.states.size() - 2);
         return n;
         
     }
@@ -120,7 +135,7 @@ public class Thompson{
         NFA result = new NFA(n.states.size() + m.states.size() + 2);
 
         // the branching of q0 to beginning of n
-        result.transitions.add(new Trans(0, 1, '_'));
+        result.transitions.add(new Trans("q0", "q1", '_'));
         
         // copy existing transisitons of n
         for (Trans t: n.transitions){
@@ -129,11 +144,11 @@ public class Thompson{
         }
         
         // transition from last n to final state
-        result.transitions.add(new Trans(n.states.size(),
-            n.states.size() + m.states.size() + 1, '_'));
+        result.transitions.add(new Trans("q"+Integer.toString(n.states.size()),
+            "q"+Integer.toString(n.states.size() + m.states.size() + 1), '_'));
 
         // the branching of q0 to beginning of m
-        result.transitions.add(new Trans(0, n.states.size() + 1, '_'));
+        result.transitions.add(new Trans("q0", "q"+Integer.toString(n.states.size() + 1), '_'));
 
         // copy existing transisitons of m
         for (Trans t: m.transitions){
@@ -142,11 +157,11 @@ public class Thompson{
         }
         
         // transition from last m to final state
-        result.transitions.add(new Trans(m.states.size() + n.states.size(),
-            n.states.size() + m.states.size() + 1, '_'));
+        result.transitions.add(new Trans("q"+Integer.toString(m.states.size() + n.states.size()),
+            "q"+Integer.toString(n.states.size() + m.states.size() + 1), '_'));
        
         // 2 new states and shifted m to avoid repetition of last n & 1st m
-        result.final_state = n.states.size() + m.states.size() + 1;
+        result.final_state = "q"+Integer.toString(n.states.size() + m.states.size() + 1);
         return result;
     }
 
@@ -311,7 +326,7 @@ public class Thompson{
             if (line.equals(":q") || line.equals("QUIT"))
                 break;
             NFA nfa_of_input = compile(line);
-            System.out.println("\nNFA:");
+            System.out.println("\nTransiciones:");
             nfa_of_input.display();
         }
     }
