56431d0c2e79e335b2638db60a4d88acd4660e12
Unknown <peinrinegan18@gmail.com>
2018-10-24 00:29:58 -0300

    probando un metodo

--- src/javaapplication5/AFD.java
+++ src/javaapplication5/AFD.java
@@ -32,6 +32,8 @@ public class AFD extends AFND
     private int estadoInicial;
     private int estadoFinal;
     private Map map;
+   private ArrayList<Integer>AFDArrayList;
+   private ArrayList<Trans>AFDTransiciones;
             
     //private Map<List<Integer>,List<List<Integer>>> map;
     
@@ -47,7 +49,7 @@ public class AFD extends AFND
         estadoFinal = afnd.final_state;
         estadoK = new EstadoAFD();
         estadosV = new ArrayList<EstadoAFD>();
-        
+        estados=new ArrayList<>();
         
         
         map=new HashMap<>();
@@ -55,8 +57,43 @@ public class AFD extends AFND
         
         alphabet=Thompson.inputAlphabet;
         alphabet.remove(0);
-        crearTablaAFD(afnd);
-        
+        //crearTablaAFD(afnd);
+       AFDArrayList= new ArrayList<>();
+       AFDTransiciones= new ArrayList<>();
+       AFDArrayList.add(0);
+       for(int i =0;i<afnd.states.size();i++)
+       {
+           for(int j=0;j<alphabet.size();j++)
+            {
+                CrearAFD(afnd.states.get(i),alphabet.get(j));
+            }
+       }
+       
+       boolean rep=false;
+       for(int i =0;i<afnd.transitions.size();i++)
+       {
+           
+                for(int j=0;j<AFDArrayList.size();j++)
+                {
+                    if(afnd.transitions.get(i).state_to == AFDArrayList.get(j))
+                    {
+                        rep=true;
+                    }
+                }
+                if(rep==false)
+                {                
+                    AFDArrayList.add(afnd.transitions.get(i).state_to);
+                    System.out.println("Vamos a agregar al AFD: "+AFDArrayList.get(i));                
+                }
+                //System.out.println("q"+AFD.get(i));
+            
+       }
+        for(int i =0;i<AFDTransiciones.size();i++)
+        {
+
+            System.out.println("("+"q"+AFDTransiciones.get(i).state_from +", "+ AFDTransiciones.get(i).trans_symbol +
+                ", "+"q"+ AFDTransiciones.get(i).state_to +")");
+        }
         
      }
     
@@ -70,8 +107,8 @@ public class AFD extends AFND
             estadosV.add(new EstadoAFD());
             for(int j=0; j < estadoK.getEstados().size(); j++)
             {
-                estadosV.get(i).getEstados().add(estadoK.getEstados().get(j));
-                estadosV.add(i, crearEstadoAFD(estadoK.getEstados().get(j), alphabet.get(i)));
+                estadosV.get(i).getEstados().add(estadoK.getEstados().get(j));///???
+                estadosV.add(i,crearEstadoAFD(estadoK.getEstados().get(j), alphabet.get(i)));
                 System.out.println("estadoV " + j + ": " + estadosV.get(j).getEstados().toString());
                                 
                 //crearEstadoAFD(afnd.states.get(i),alphabet.get(j));
@@ -87,7 +124,7 @@ public class AFD extends AFND
         estados = afnd.getNextStates(afnd, estado, c);
         estadoAFD.setEstados(estados);
         System.out.println("Estadoss:"+estados.toString());
-        Collections.sort(estados);
+        //Collections.sort(estados);
         if(estados.contains(estadoInicial))
         {
             estadoAFD.setEstadoInicial(true);
@@ -101,5 +138,18 @@ public class AFD extends AFND
         return estadoAFD;
     }
     
-    
+    public void CrearAFD(int estado,Character c)
+    {
+        for(int i =0;i<afnd.transitions.size();i++)
+        {
+            if(afnd.transitions.get(i).state_from ==estado && afnd.transitions.get(i).trans_symbol ==c)
+            {
+
+                AFDArrayList.add(transitions.get(i).state_to);
+                AFDTransiciones.add(transitions.get(i));
+                CrearAFD(afnd.transitions.get(i).state_to,c);        
+                //System.out.println("q"+AFD.get(i));
+            }
+        }
+    }
 }
